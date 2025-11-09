c244812b6e0a32772892571d9e785e962d76082f
Leonardo Castro <twelve.programming@gmail.com>
2025-11-08 21:33:45 -0400

    Adicionando a versão final dos códigos de rasterização de linhas pelo método analítico

--- trabalho1/metodo_analitico_linhas.py
+++ trabalho1/metodo_analitico_linhas.py
@@ -1,5 +1,4 @@
-#import matplotlib.pyplot as plt
-#import numpy as np
+from math import floor, ceil
 
 def menu():
     num_linha, num_coluna = map(int, input("Escolha o tamanho da matriz(ex: 3x3): ").split("x"))
@@ -23,22 +22,29 @@ def gerar_matriz_de_pixels(num_linha, num_coluna):
     return matriz
 
 def mostrar_matriz(matriz):
-    for linha in reversed(matriz): # inverte fazendo o pixel 0,0 ser o do canto inferior esquerdo
-        print(" ".join(linha))  # Deixa mais bonito (sem colchetes)
+    for linha in reversed(matriz): #inverte fazendo o pixel 0,0 ser o do canto inferior esquerdo
+        print(" ".join(linha))  #deixa mais bonito (sem colchetes)
 
 def liga_pixel(x, y, matriz):
-    if 0 <= y < len(matriz) and 0 <= x < len(matriz[0]): #Verifica se tá dentro da matriz
+    if 0 <= y < len(matriz) and 0 <= x < len(matriz[0]): #verifica se tá dentro da matriz
         matriz[y][x] = "⬛" 
 
 def gerar_reta(x1, y1, x2, y2, matriz):
     if x1 == x2:
-        for y in range(y1, y2 + 1): #Vai pecorrer a distância da diferença dos pontos em relação ao y
+        for y in range(y1, y2 + 1): #vai pecorrer a distância da diferença dos pontos em relação ao y
             liga_pixel(x1, y, matriz)
     else: #caso não seja vertical
         m = (y2 - y1) / (x2 - x1) #coeficiente angular
         b = y1 - m*x1 #coeficiente linear
         for x in range(x1, x2 + 1): #Vai pecorrer a distância da diferença dos pontos em relação ao x
-            y = int(round(m * x + b)) #Vai pegar o valor de y para cada ponto entre x1 e x2 e arredondar
+            y = m * x + b #Vai pegar o valor de y para cada ponto entre x1 e x2
+            y = arredondar(y)
             liga_pixel(x, y, matriz) #liga_pixel(x, y, cor)
+
+def arredondar(y):
+    if y >= 0:
+        return floor(y + 0.5) #aqui é para números inteiros, quando for mas que 0.5 vai para próxima casa decimal e arredonda para baixo se for menor ou igual que 0.4 vai ficar no máximo 0.9 então vai para casa decimal anterior
+    else:
+        return ceil(y - 0.5)
 
 menu()
